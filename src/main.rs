27867103b0b40f09e868b4cd8cd7e956ace647bd
Vlad-Stefan Harbuz <vlad@vladh.net>
2024-09-28 19:46:02 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use iced::widget::{button, text};
+use iced::Element;
+
+#[derive(Debug, Clone)]
+enum Message {
+    Increment,
+}
+
+pub fn main() -> iced::Result {
+    iced::run("A cool counter", update, view)
+}
+
+fn update(counter: &mut u64, message: Message) {
+    match message {
+        Message::Increment => *counter += 1,
+    }
+}
+
+fn view(counter: &u64) -> Element<Message> {
+    button(text(counter)).on_press(Message::Increment).into()
+}
